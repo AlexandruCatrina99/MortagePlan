f1643749156279812bc44e85fb3bd5a4e3c7610a
AlexandruCatrina99 <77682423+AlexandruCatrina99@users.noreply.github.com>
2022-02-13 21:31:43 +0200

    Update

--- src/main/java/fi/crosskey/mortagePlan/Controller.java
+++ src/main/java/fi/crosskey/mortagePlan/Controller.java
@@ -25,7 +25,18 @@ public class Controller {
         prospects= readFile("prospects.txt");
     }
 
-
+//    // Annotation
+//    @PutMapping("/change/{id}")
+//
+//    // Method
+//    void change(@PathVariable int id) {
+//
+//        System.out.println(ans);
+//        ans.remove(id);
+//        ans.add(id, 45);
+//
+//        System.out.println(ans);
+//    }
 
     @PutMapping("prospects/{name}/{loan}/{interest}/{years}")
     void addProspect(@PathVariable String name,@PathVariable double loan,@PathVariable double interest , @PathVariable int years ) throws IOException {
@@ -44,7 +55,7 @@ public class Controller {
     // Method
     ArrayList<Prospect> getProspects() {
         for (int i =0 ; i<prospects.size();i++
-             ) {
+        ) {
             System.out.println("*************************************************************");
             System.out.println("Prospect " + (i+1)+": " + prospects.get(i).getName() + " wants to borrow " +prospects.get(i).getLoan() + " € for a period of " + prospects.get(i).getYears() + " years and pay" +
                     String.format("%.2f", prospects.get(i).getMonthlyPayment()) + "€ each month");
--- src/main/java/fi/crosskey/mortagePlan/MortagePlanApplication.java
+++ src/main/java/fi/crosskey/mortagePlan/MortagePlanApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MortagePlanApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(MortagePlanApplication.class, args);
 	}
